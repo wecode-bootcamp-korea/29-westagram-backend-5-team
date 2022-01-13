3357a9c10b1c5b5bbdac67f89ec4b7dc610f1616
wepungsan <wepungsan@gmail.com>
2022-01-13 16:44:37 +0900

    add email max_length

--- students/dowoon/users/models.py
+++ students/dowoon/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name       = models.CharField(max_length=25)
-    email      = models.EmailField(unique=True)
+    email      = models.EmailField(unique=True, max_length=50)
     password   = models.CharField(max_length=200)
     phone      = models.CharField(max_length=25)
     created_at = models.DateTimeField(auto_now_add=True)
