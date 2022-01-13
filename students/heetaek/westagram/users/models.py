1c82032dc3cba2931535049531f7b535274e9995
sosolyht <sosolyht@gmail.com>
2022-01-13 16:21:52 +0900

    [Modify] change password length 50 to 200 , add unique attribute

--- students/heetaek/westagram/users/models.py
+++ students/heetaek/westagram/users/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class User(models.Model):
     name        = models.CharField(max_length=30)
-    email       = models.CharField(max_length=50)
-    password    = models.CharField(max_length=50)
-    phone       = models.CharField(max_length=20)
+    email       = models.CharField(max_length=50, unique=True)
+    password    = models.CharField(max_length=200)
+    phone       = models.CharField(max_length=20, unique=True)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
     
