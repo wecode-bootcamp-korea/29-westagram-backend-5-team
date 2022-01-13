ee83dbee0aa5e3c4d0d5b748106d326dc5dc2164
sosolyht <sosolyht@gmail.com>
2022-01-13 16:18:50 +0900

    [Modify] fix convation

--- students/heetaek/westagram/users/models.py
+++ students/heetaek/westagram/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone = models.CharField(max_length=20)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name        = models.CharField(max_length=30)
+    email       = models.CharField(max_length=50)
+    password    = models.CharField(max_length=50)
+    phone       = models.CharField(max_length=20)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = "users"
